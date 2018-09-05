421b858cd4789a6efa0d7cf9e51ab9e8bd4d0598
tomtiddler <15111171986@163.com>
2018-09-05 23:53:31 +0800

    配置单项get的查询参数，由于对于单个用户，单个商品的收藏数是唯一的，所以lookup_field设置为goods_id。

--- apps/user_operation/views.py
+++ apps/user_operation/views.py
@@ -11,7 +11,7 @@ from utils.permissions import IsOwnerOrReadOnly
 
 
 # Create your views here.
-class UserFavViewset(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
+class UserFavViewset(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
     """
     用户收藏功能
     """
@@ -19,6 +19,7 @@ class UserFavViewset(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.Dest
     authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)  # 对象级别认证
     permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)  # 验证是否为登录用户,拥有的用户。
     serializer_class = UserFavSerializer
+    lookup_field = "goods_id"  # 单项查找时的搜索字段，默认为pk，可能表示model_id。查询是在query_set之后，已经经过了过滤。
 
     def get_queryset(self):  # 此过滤条件暂时可以直接写入queryset的过滤。
         return self.queryset.filter(user=self.request.user)
